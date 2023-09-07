15770ca003d913d10d98dd1c4c0bd8b5402de1e1
Ojo Ayodeji <degzie24@gmail.com>
2023-09-07 13:02:09 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const date = new Date();
+    const currentDayStr = daysOfWeek[date.getDay()];
+
+
+function getCurrentUTC() {
+    const currentDate = new Date();
+    const currentUTCTime = currentDate.toISOString();
+
+    return currentUTCTime;
+};
+
+const utc_time = getCurrentUTC();
+
+app.get('/api', (req,res) =>{
+    const slack_name = req.query.slack_name;
+    const track = req.query.track;
+
+    res.status(200).json({
+        slack_name,
+        current_day: currentDayStr,
+        utc_time,
+        track,
+        github_file_url: 'linktofile',
+        github_repo_url: 'linktorepo',
+        status_code: 200
+    });
+});
+
+module.exports = app;
